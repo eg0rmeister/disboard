94766f59434059c10b37d721b1e2faaf8cf901a1
Egor Karpukhin <karpuhin.egor@gmail.com>
2023-06-30 18:32:56 +0300

    partly done bot_manager

---
+++ src/bot_manager.py
@@ -0,0 +1,55 @@
+# bot.py
+import os
+
+import discord
+import dotenv
+import asyncio
+
+class Interacter:
+  client = None
+  online = False
+  master_id = None
+  main_task = None
+  master_vc = None
+  vc = None
+
+  async def Update(self):
+
+    # Load and assign environment variables
+    dotenv.load_dotenv()
+    token = os.getenv("DISCORD_TOKEN")[1:-1]
+    self.master_id = int(os.getenv("MY_MASTER_ID")[1:-1])
+
+    # Create client objects with all intents
+    intent = discord.Intents.all()
+    self.client = discord.Client(intents=intent)
+    
+    # Start client and wait until it is started
+    @self.client.event
+    async def on_ready():
+      self.online = True
+      # await self.client.close()
+
+    await self.client.login(token)
+    self.main_task = asyncio.ensure_future(self.client.connect(reconnect=False))
+    while (not self.online):
+      await asyncio.sleep(0.5)
+
+  async def SendMessage(self):
+    message = "hello"
+    chat = await self.client.create_dm(self.client.get_user(self.master_id))
+    await chat.send(message)
+    return
+  
+  def FindFigula(self):
+    for guild in self.client.guilds:
+      for member in guild.members:
+        if (member.id == self.master_id and member.voice is not None):
+          self.master_vc = member.voice
+          return
+
+  async def ConnectToFigula(self):
+    self.vc = await self.master_vc.channel.connect()
+  
+  async def Disconnect(self):
+    await self.vc.disconnect()
